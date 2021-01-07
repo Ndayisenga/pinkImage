7c78002df05df52f811f6cd08b30d7f44697112c
John <cloude056@yahoo.fr>
2021-01-07 13:17:23 +0200

    Pink Image .

--- pinkImage/ContentView.swift
+++ pinkImage/ContentView.swift
@@ -8,9 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var isHearted = false
+    @State private var numHearts = 999
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            Image("pinkbuilding").resizable().cornerRadius(30).padding().shadow(radius: 10)
+            
+            HStack {
+                
+                Button(action: {
+                    self.isHearted.toggle()
+                    
+                    if self.isHearted {
+                        self.numHearts += 1
+                    }
+                    else {
+                        self.numHearts -= -1
+                    }
+                }) {
+                    
+                    if isHearted {
+                        
+                        Image(systemName: "heart.fill")
+                    }
+                    else {
+                        Image(systemName: "heart")
+                    }
+                    
+                    
+                }
+                
+                Text(String(numHearts))
+            }
+            
+            
+            
+            
+            
+            Text("This pink building is wow ").fontWeight(.heavy).foregroundColor(.pink).font(.largeTitle).multilineTextAlignment(.center).shadow(color:.black , radius: 0.1, x: 2, y: 2 )
+        }
     }
 }
 
